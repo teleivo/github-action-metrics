43ea87e02d314920b29ffe4245f8f7c3ea3a52f5
teleivo <teleivo@users.noreply.github.com>
2021-10-23 13:31:54 +0200

    sq

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,34 @@
 import { Octokit } from "octokit";
-import { openSync, writeFileSync} from "fs";
+import { openSync, closeSync, existsSync, writeFileSync} from "fs";
 
 const octokit = new Octokit();
 
 async function fetchWorkflows() {
     const file = "data/workflows.json"
-
-    const fd=openSync(file, "w")
-
-    const response = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows', {
-        owner: 'dhis2',
-        repo: 'dhis2-core'
-    })
-
-    if (response.status === 200){
-        writeFileSync(fd, JSON.stringify(response.data))
+    if (existsSync(file)) {
+        console.log(`workflows exist already in ${file}`)
+        return
     }
+
+    let response
+    try {
+        response = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows', {
+            owner: 'dhis2',
+            repo: 'dhis2-core'
+        })
+    } catch (error) {
+        console.error(error)
+        return 
+    }
+
+    if (response.status != 200){
+        console.log(`failed to fetch workflows API responded with status ${response.status}`)
+        return
+    }
+
+    const fd = openSync(file, "w")
+    writeFileSync(fd, JSON.stringify(response.data))
+    closeSync(fd)
 }
 
 fetchWorkflows()
