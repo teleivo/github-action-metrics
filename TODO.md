afe6ad83df8017be8bb7686778636153415d7591
teleivo <teleivo@users.noreply.github.com>
2021-10-24 07:29:17 +0200

    fetch runs for run tests workflow

--- TODO.md
+++ TODO.md
@@ -14,11 +14,9 @@ curl \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/dhis2/dhis2-core/actions/workflows/10954/runs
 
-This reduces the number of runs.
-
-via
-
-https://docs.github.com/en/rest/reference/actions#list-workflow-runs-for-a-repository
+* what if the total number changes? how to first get the total number, check
+  how many I have already stored and only fetch whats left?
+* get job for each run
 
 limit myself to a few workflow_id's 
 
@@ -28,6 +26,12 @@ limit myself to a few workflow_id's
 then for each run fetch the "jobs_url" inside the run
 (https://docs.github.com/en/rest/reference/actions#get-a-job-for-a-workflow-run)
 
+* if I start with fetchAllRuns() and store the last page in data/workflow/10954/runs/lastPage
+I can resume using the lastPage just in case the lastPage was not full
+
 * autoformat code
 * create a small CLI out of it so I can easily get all runs for other workflows
 * then get jobs for runs?
+* setup ELK stack
+* ingest data
+* create first visualization for for example how did the "Run integration tests" step evolve over time?
--- src/runs.ts
+++ src/runs.ts
@@ -5,34 +5,43 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 });
 
-async function fetchRuns() {
-    const file = "data/runs.json"
-    if (existsSync(file)) {
-        console.log(`runs exist already in ${file}`)
-        return
-    }
+async function fetchAllRuns(workflow_id : number) {
+    octokit.hook.after("request", async (response, options) => {
+        const ratelimit = `${response.headers["x-ratelimit-used"]}/${response.headers["x-ratelimit-limit"]}`
+        console.log(`requested ${options.method} ${options.url}: ${response.status} ratelimit ${ratelimit}`)
+    })
+    octokit.hook.error("request", async (error, _) => {
+        console.error(error)
+    })
 
-    let response
     try {
-        response = await octokit.request('GET /repos/{owner}/{repo}/actions/runs', {
-            owner: 'dhis2',
-            repo: 'dhis2-core',
-            event: 'pull_request',
-            status: 'completed'
+        const iterator = octokit.paginate.iterator(octokit.rest.actions.listWorkflowRuns, {
+            owner: "dhis2",
+            repo: "dhis2-core",
+            event: "pull_request",
+            status: "completed",
+            workflow_id,
+            per_page: 100,
         })
+
+        for await (const { data: runs } of iterator) {
+            for (const run of runs) {
+                console.log("Run #%d", run.id);
+                const file = `data/workflows/${workflow_id}/runs/${run.id}.json`
+                if (existsSync(file)) {
+                    console.log("run #%d exist already in %s", run.id, file)
+                    continue
+                }
+                const fd = openSync(file, "w")
+                writeFileSync(fd, JSON.stringify(run))
+                closeSync(fd)
+            }
+        }
     } catch (error) {
         console.error(error)
         return 
     }
-
-    if (response.status != 200){
-        console.log(`failed to fetch runs API responded with status ${response.status}`)
-        return
-    }
-
-    const fd = openSync(file, "w")
-    writeFileSync(fd, JSON.stringify(response.data))
-    closeSync(fd)
 }
 
-fetchRuns()
+const workflowId = 10954
+fetchAllRuns(workflowId)
