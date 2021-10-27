5cb224a7f3b159c51a9a0b26e9d76903a34a8f6f
teleivo <teleivo@users.noreply.github.com>
2021-10-27 06:09:16 +0200

    extract params, rename and document

--- src/runs.ts
+++ src/runs.ts
@@ -1,11 +1,20 @@
 import { Octokit } from "octokit";
 import { openSync, closeSync, existsSync, writeFileSync } from "fs";
+import { RestEndpointMethodTypes } from "@octokit/plugin-rest-endpoint-methods";
 
 const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 });
 
-async function fetchAllRuns(workflow_id: number) {
+// Although not specified in the docs https://docs.github.com/en/rest/reference/actions#list-workflow-runs
+// the API only returns the 10 last pages. So we can get the last ~1000 runs
+// with per_page set to 100. Older ones can be fetched using the parameter
+// created https://github.blog/changelog/2021-09-02-github-actions-filter-workflow-runs-by-created-date/
+// You can thus run this every day and it will store the latest runs you have
+// not yet stored locally.
+async function fetchLatestRuns(
+  params: RestEndpointMethodTypes["actions"]["listWorkflowRuns"]["parameters"]
+) {
   octokit.hook.after("request", async (response, options) => {
     const ratelimit = `${response.headers["x-ratelimit-used"]}/${response.headers["x-ratelimit-limit"]}`;
     console.log(
@@ -20,11 +29,9 @@ async function fetchAllRuns(workflow_id: number) {
     const iterator = octokit.paginate.iterator(
       octokit.rest.actions.listWorkflowRuns,
       {
-        owner: "dhis2",
-        repo: "dhis2-core",
+        ...params,
         event: "pull_request",
         status: "completed",
-        workflow_id,
         per_page: 100,
       }
     );
@@ -32,7 +39,7 @@ async function fetchAllRuns(workflow_id: number) {
     for await (const { data: runs } of iterator) {
       for (const run of runs) {
         console.log("Run #%d", run.id);
-        const file = `data/workflows/${workflow_id}/runs/${run.id}.json`;
+        const file = `data/workflows/${params.workflow_id}/runs/${run.id}.json`;
         if (existsSync(file)) {
           console.log("run #%d already exists in %s", run.id, file);
           continue;
@@ -48,5 +55,9 @@ async function fetchAllRuns(workflow_id: number) {
   }
 }
 
-const workflowId = 10954;
-fetchAllRuns(workflowId);
+const params = {
+  owner: "dhis2",
+  repo: "dhis2-core",
+  workflow_id: 10954,
+};
+fetchLatestRuns(params);
